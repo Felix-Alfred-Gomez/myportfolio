1cf04d1dd845b7eff04e2de26773b8b5bb410da0
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-04-23 17:52:01 +0200

    push not-working-version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import Home from './pages/Home';
+//import Login from './pages/Login';
+//import Signup from './pages/Signup';
+//import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/connection" element={<Login />} />
-        <Route path="/inscription" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    //<Router>
+    //  <Routes>
+    //    <Route path="/" element={<Home />} />
+    //    <Route path="/connection" element={<Login />} />
+    //    <Route path="/inscription" element={<Signup />} />
+    //    <Route path="/dashboard" element={<Dashboard />} />
+    //  </Routes>
+    //</Router>
+    <div></div>
   );
 }
 
