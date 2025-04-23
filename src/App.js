1d517c581ba416dbe76615693b638a833e8cb85f
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-04-23 19:23:56 +0200

    restart from scratch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import Home from './pages/Home';
-//import Login from './pages/Login';
-//import Signup from './pages/Signup';
-//import Dashboard from './pages/Dashboard';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    //<Router>
-    //  <Routes>
-    //    <Route path="/" element={<Home />} />
-    //    <Route path="/connection" element={<Login />} />
-    //    <Route path="/inscription" element={<Signup />} />
-    //    <Route path="/dashboard" element={<Dashboard />} />
-    //  </Routes>
-    //</Router>
-    <div></div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
