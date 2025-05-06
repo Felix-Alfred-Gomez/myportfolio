201c48549f525781e5c8d4e0cdc3aa3aec72d98b
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-05-06 14:12:21 +0200

    Refactor code with nav bar

--- src/components/common/LoginButton.jsx
+++ src/components/common/LoginButton.jsx
@@ -1,4 +1,4 @@
-import {React, useContext, useState} from "react";
+import {React, useContext} from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext"; // Importer le contexte
 import "../../styles/common.css"; // Import the common CSS file
@@ -6,28 +6,27 @@ import "../../styles/common.css"; // Import the common CSS file
 function LoginButton({ onLoginClick, onLogoutClick }) {
   const navigate = useNavigate();
   const { isAuthenticated } = useContext(AuthContext);
-  // State to track hover status
-  const [isHovered, setIsHovered] = useState(false);
 
   const handleLogout = () => {
     onLogoutClick(); // Trigger the logout function
     navigate("/"); // Redirect to the home page
   };
 
+  const handleLogin = () => {
+    onLoginClick(); // Trigger the logout function
+    navigate("/dashboard"); // Redirect to the home page
+  };
+
   return isAuthenticated ? (
     <button 
-    className={`button ${isHovered ? "hovered" : ""}`}
-    onMouseEnter={() => setIsHovered(true)} // Inline hover logic
-    onMouseLeave={() => setIsHovered(false)} // Inline hover logic
+    className="connect_button"
     onClick={handleLogout}>
       Se Déconnecter
     </button>
   ) : (
     <button 
-    className={`button ${isHovered ? "hovered" : ""}`}
-    onMouseEnter={() => setIsHovered(true)} // Inline hover logic
-    onMouseLeave={() => setIsHovered(false)} // Inline hover logic
-    onClick={onLoginClick}>
+    className="connect_button"
+    onClick={handleLogin}>
       Se Connecter
     </button>
   );
