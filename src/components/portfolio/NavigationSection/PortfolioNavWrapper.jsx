408b7d70429920488dd45f193c047cf6166b1321
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-05-23 14:02:43 +0200

    Design a navigation wrapper for nav bar and side menu

---
+++ src/components/portfolio/NavigationSection/PortfolioNavWrapper.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import DesignOptionsModal from "./DesignOptionsModal";
+import BurgerIcon from "./BurgerIcon";
+import SideMenu from "./SideMenu";
+import MainNav from "./MainNav";
+import { Cog6ToothIcon } from '@heroicons/react/24/solid';
+
+export default function PortfolioNavWrapper({
+  showDesignModal,
+  setShowDesignModal,
+  menuOpen,
+  setMenuOpen,
+  menuRef,
+  burgerRef
+}) {
+  return (
+    <>
+      <button
+        className="button-template-option-wheel"
+        title="Options"
+        onClick={() => setShowDesignModal(true)}>
+        <Cog6ToothIcon className="option-wheel-icon" />
+      </button>
+
+      {/* Design Options Modal */}
+      <DesignOptionsModal
+        show={showDesignModal}
+        onClose={() => setShowDesignModal(false)}
+      />
+
+      {/* Burger Icon */}
+      <BurgerIcon
+        ref={burgerRef}
+        onClick={() => setMenuOpen(!menuOpen)}
+        isOpen={menuOpen}
+      />
+
+      {/* Side menu */}
+      <SideMenu
+        menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}
+        menuRef={menuRef}
+        burgerRef={burgerRef}
+      />
+
+      {/* Main Navigation */}
+      <MainNav />
+    </>
+  );
+}
--- src/components/portfolio/PortfolioTemplate.jsx
+++ src/components/portfolio/PortfolioTemplate.jsx
@@ -3,13 +3,9 @@ import { useParams, useNavigate } from "react-router-dom";
 import { GetPortfolioData, PushPortfolioData } from "../../hooks/HandlePortfolioData";
 import SkillsSection from "./SkillsSection/SkillsSection";
 import AccueilSection from "./AccueilSection/AccueilSection";
-import BurgerIcon from "./BurgerIcon";
-import SideMenu from "./SideMenu";
 import PublishModal from "./PublishModal";
-import MainNav from "./MainNav";
-import DesignOptionsModal from "./DesignOptionsModal";
+import PortfolioNavWrapper from "./NavigationSection/PortfolioNavWrapper";
 import "../../styles/PortfolioTemplate.css";
-import { Cog6ToothIcon } from '@heroicons/react/24/solid';
 
 export function PortfolioContent({ isPublished }) {
   const { username } = useParams();
@@ -42,34 +38,16 @@ export function PortfolioContent({ isPublished }) {
             Publier
           </button>
 
-          <button
-            className="button-template-option-wheel"
-            title="Options"
-            onClick={() => setShowDesignModal(true)}>
-            <Cog6ToothIcon className="option-wheel-icon" />
-          </button>
-
           <button className="button-template return" onClick={() => navigate("/dashboard")}>
             Retour
           </button>
         </>
       )}
 
-      {/* Design Options Modal */}
-      <DesignOptionsModal
-        show={showDesignModal}
-        onClose={() => setShowDesignModal(false)}
-      />
-
-      {/* Burger Icon */}
-      <BurgerIcon
-        ref={burgerRef}
-        onClick={() => setMenuOpen(!menuOpen)}
-        isOpen={menuOpen}
-      />
-
-      {/* Side menu */}
-      <SideMenu
+      {/* Navigation Wrapper */}
+      <PortfolioNavWrapper
+        showDesignModal={showDesignModal}
+        setShowDesignModal={setShowDesignModal}
         menuOpen={menuOpen}
         setMenuOpen={setMenuOpen}
         menuRef={menuRef}
@@ -82,9 +60,6 @@ export function PortfolioContent({ isPublished }) {
         url={portfolioUrl}
         onClose={() => setShowModal(false)}
       />
-
-      {/* Main Navigation - hidden on small screens */}
-      <MainNav />
 
       <AccueilSection
         username={username}
