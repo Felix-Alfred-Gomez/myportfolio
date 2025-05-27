f6b1c5feba78bb29f770c41336119952b15750c2
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-05-27 12:29:45 +0200

    handlenestedfield function

--- src/components/common/fieldHandlers.jsx
+++ src/components/common/fieldHandlers.jsx
@@ -1,4 +1,9 @@
-// function to handle changes in input fields
+// functions to handle field changes in forms
 export const handleFieldChange = (setData, data, field) => (e) => {
   setData({ ...data, [field]: e.target.value });
+};
+
+// function to handle changes in nested fields
+export const handleNestedFieldChange = (setData, data, parentField, field) => (value) => {
+  setData({...data, [parentField]: {...data[parentField], [field]: value}});
 };
