6fe822c2f7b42ef42db86e45e6c4a35acbca72c3
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-06-10 11:03:54 +0200

    Enhance user experience and import unit tests for deepmerge

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
 // filepath: h:\CODE\myportfolio\src\components\Dashboard\Dashboard.js
-import { useEffect, useState, useContext} from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import LoginButton from "../common/LoginLogo"; 
-import { AuthContext, FetchUsername} from "../../context/AuthContext"; 
-import LogoButton from "../common/WebsiteLogo"; 
+import LoginButton from "../common/LoginLogo";
+import { AuthContext, FetchUsername } from "../../context/AuthContext";
+import LogoButton from "../common/WebsiteLogo";
 import dashboardBackground from "../../assets/dashboard.png";
 import { getAuth, signOut } from "firebase/auth";
 import { TiTick } from "react-icons/ti";
@@ -13,7 +13,7 @@ function Dashboard() {
   const navigate = useNavigate();
 
   const { setIsAuthenticated } = useContext(AuthContext);
-  const [ setShowLogin] = useState(false);
+  const [setShowLogin] = useState(false);
   // Function to modify the showLogin state and display/hide the login modal
   const toggleLoginModal = (state) => setShowLogin(state);
 
@@ -48,8 +48,8 @@ function Dashboard() {
 
   // const handleViewPortfolio = () => {
   //   if (username) {
-      // navigate(`/${username}`);
-      // Open the portfolio in a new tab
+  // navigate(`/${username}`);
+  // Open the portfolio in a new tab
   //     window.open(`/${username}`, "_blank", "noopener,noreferrer");
   //   } else {
   //     alert("Veuillez vous connecter pour voir votre portfolio.");
@@ -62,31 +62,17 @@ function Dashboard() {
 
         <nav className="nav-site">
           <LogoButton />
-          <LoginButton 
-            onLoginClick={() => toggleLoginModal(true)} 
-            onLogoutClick={() => handleAuthChange(false)} 
+          <LoginButton
+            onLoginClick={() => toggleLoginModal(true)}
+            onLogoutClick={() => handleAuthChange(false)}
           />
         </nav>
 
         <img src={dashboardBackground} alt="Background" className="hero-background" />
-        
+
         <div className="hero-overlay-dashboard">
           {/* <h2>Dashboard</h2> */}
           <h2>Créez votre portfolio web personnalisé en quelques clics</h2>
-          <ul className="dashboard-features-list">
-            <li>
-              <span className="feature-tick"><TiTick /></span>
-              <span>Présentez vos projets en quelques secondes</span>
-            </li>
-            <li>
-              <span className="feature-tick"><TiTick /></span>
-              Personnalisez chaque section avec des images, des textes et des liens
-            </li>
-            <li>
-              <span className="feature-tick"><TiTick /></span>
-              Partagez votre portfolio public avec votre adresse url personnelle
-            </li>
-          </ul>
 
           <div className="buttons-group">
             <button
@@ -101,6 +87,21 @@ function Dashboard() {
               Voir mon Portfolio
             </button> */}
           </div>
+
+          <ul className="dashboard-features-list">
+            <li>
+              <span className="feature-tick"><TiTick /></span>
+              <span>Présentez vos projets en quelques secondes</span>
+            </li>
+            <li>
+              <span className="feature-tick"><TiTick /></span>
+              Personnalisez chaque section avec des images, des textes et des liens
+            </li>
+            <li>
+              <span className="feature-tick"><TiTick /></span>
+              Partagez votre portfolio public avec votre adresse url personnelle
+            </li>
+          </ul>
         </div>
       </section>
     </div>
